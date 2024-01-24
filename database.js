80de8ebd524bcf394c5b1985d1378d58bb06e854
Elias <eliasespondaburucontacto0@gmail.com>
2024-01-24 09:39:23 -0300

    changes in database.js v2

--- database.js
+++ database.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
+const connectionString = process.env.MONGODB_URI;
+
+// Log de las credenciales antes de la conexión
+const credentialsMatch = connectionString.match(/\/\/(.*):(.*)@/);
+if (credentialsMatch) {
+  const username = credentialsMatch[1];
+  const password = credentialsMatch[2];
+  console.log(`Usuario: ${username}, Contraseña: ${password}`);
+}
+
+
 // Conectarse a MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI);
+
 
 const db = mongoose.connection;
 
